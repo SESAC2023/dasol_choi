f400a07b5f827e15e6afccb14242bbf53b194eda
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-18 22:46:20 +0900

    Create 2588.py

---
+++ BOJ/단계별로풀어보기/1.입출력과 사칙연산/2588.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = input()
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
+
+
+# a = int(input())
+# b = input()
+# b_1, b_2, b_3 = map(int, b)
+
+# print(a*b_3)
+# print(a*b_2)
+# print(a*b_1)
+# print(a*int(b))
