fde7de899c9fc3371d5cc89b60c5aea9050856f8
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-18 23:14:10 +0900

    Create 25304.py

---
+++ BOJ/단계별로풀어보기/3.반복문/25304.py
@@ -0,0 +1,14 @@
+#영수증
+
+x = int(input())
+n = int(input())
+
+s = 0
+for i in range(n):
+    a, b = map(int, input().split())
+    s += a*b
+  
+if x == s:
+    print('Yes')
+else:
+    print('No')
