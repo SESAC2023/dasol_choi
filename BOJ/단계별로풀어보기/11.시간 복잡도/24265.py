70cbddaa4dc0333e5e6522549866d5b88c53f482
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-24 13:54:24 +0900

    Create 24265.py

---
+++ BOJ/단계별로풀어보기/11.시간 복잡도/24265.py
@@ -0,0 +1,9 @@
+#알고리즘 수업 - 알고리즘의 수행 시간 4
+
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n - 1
+#         for j <- i + 1 to n
+#             sum <- sum + A[i] × A[j]; # 코드1
+#     return sum;
+# }
