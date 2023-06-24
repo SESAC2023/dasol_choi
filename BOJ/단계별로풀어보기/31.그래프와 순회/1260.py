967d3cac3120efc13bcf55be949549003ce271c5
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-24 14:40:38 +0900

    Create 1260.py

---
+++ BOJ/단계별로풀어보기/31.그래프와 순회/1260.py
@@ -0,0 +1,42 @@
+#DFS와 BFS
+
+import sys
+from collections import deque
+sys.setrecursionlimit(int(1e6))
+input = sys.stdin.readline
+
+n, m, v = map(int, input().split())
+
+graph = [[] for i in range(n + 1)]
+
+dfs_visited = [False] * (n + 1)
+bfs_visited = [False] * (n + 1)
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n+1):
+  graph[i].sort()
+
+def dfs(x):
+  dfs_visited[x] = True
+  print(x, end=" ")
+  for y in graph[x]:
+    if not dfs_visited[y]:
+      dfs(y)
+
+def bfs(x):
+  q = deque([x])
+  bfs_visited[x] = True 
+  while q:
+    v = q.popleft()
+    print(v, end=" ")
+    for y in graph[v]:
+      if not dfs_visited[y]:
+        q.append(y)
+        bfs_visited[y] == True
+
+dfs(v)
+bfs(v)
