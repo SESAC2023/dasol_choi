92f1acc615dcbdaae921ca9ba3c4cb6aec6ccded
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-24 14:50:23 +0900

    Update 1260.py

--- BOJ/단계별로풀어보기/31.그래프와 순회/1260.py
+++ BOJ/단계별로풀어보기/31.그래프와 순회/1260.py
@@ -13,9 +13,9 @@ dfs_visited = [False] * (n + 1)
 bfs_visited = [False] * (n + 1)
 
 for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
 for i in range(1, n+1):
   graph[i].sort()
@@ -41,55 +41,3 @@ def bfs(x):
 dfs(v)
 print()
 bfs(v)
-
-
-""" 수정
-#DFS와 BFS
-
-import sys
-from collections import deque
-
-sys.setrecursionlimit(int(1e6))
-input = sys.stdin.readline
-
-n, m, v = map(int, input().split())
-
-graph = [[] for i in range(n + 1)]
-
-dfs_visited = [False] * (n + 1)
-bfs_visited = [False] * (n + 1)
-
-for i in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-for i in range(1, n + 1):
-    graph[i].sort()
-
-
-def dfs(x):
-    dfs_visited[x] = True
-    print(x, end=" ")
-    for y in graph[x]:
-        if not dfs_visited[y]:
-            dfs(y)
-
-
-def bfs(x):
-    q = deque([x])
-    bfs_visited[x] = True
-    while q:
-        x = q.popleft()
-        print(x, end=" ")
-        for y in graph[x]:
-            if not bfs_visited[y]:
-                q.append(y)
-                bfs_visited[y] = True
-
-
-dfs(v)
-print()
-bfs(v)
-
-"""
