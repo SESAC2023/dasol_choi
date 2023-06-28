46021e32eaf9281dfb7471c2080a0f605cd619be
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-06-28 14:29:52 +0900

    Update 10989.py

--- BOJ/단계별로풀어보기/13.정렬/10989.py
+++ BOJ/단계별로풀어보기/13.정렬/10989.py
@@ -1,19 +1,17 @@
 #수 정렬하기 3
 
-# 메모리 초과 실패!!!!!!!!!!!!
-
 import sys
 input = sys.stdin.readline
 
 #계수 정렬
 n = int(input())
-li = [int(input()) for _ in range(n)]
 
-cnt = [0] * (n+1)
+cnt = [0] * 10001
 
-for i in range(n):
-  cnt[li[i]] += 1
+for _ in range(n):
+  num = int(input())
+  cnt[num ] += 1
 
-for i in range(len(cnt)):
+for i in range(10001):
   for j in range(cnt[i]):
     print(i)
