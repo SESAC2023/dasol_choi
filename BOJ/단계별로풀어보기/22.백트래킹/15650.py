6c9042cc23581599e18884d0a4c377e47e341245
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-07-01 23:28:14 +0900

    Update 15650.py

--- BOJ/단계별로풀어보기/22.백트래킹/15650.py
+++ BOJ/단계별로풀어보기/22.백트래킹/15650.py
@@ -4,25 +4,16 @@ import sys
 input = sys.stdin.readline
 #n까지 자연수 중 m개 고른 경우의 수(오름차순)
 
-def dfs():
-    if len(s) == m:
-        if sorted(s) not in ss:
-          ss.append(sorted(s))
-        return
-    for i in range(1, n+1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        s.append(i)
-        dfs()
-        s.pop()
-        visited[i] = False           
-
-n, m = map(int, input().split())
+n,m = list(map(int,input().split()))
 s = []
-ss = []
-visited = [False] * (n+1)
-
-dfs()
-for s in ss:
-  print(' '.join(map(str, s)))
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(start,n+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+dfs(1)
