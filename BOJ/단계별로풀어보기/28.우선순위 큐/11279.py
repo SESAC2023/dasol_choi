1f8e037f3cc77d2f8dd7363ed93b76c8d91bb618
Dasol-Choi <103584654+Dasol-Choi@users.noreply.github.com>
2023-07-23 14:27:09 +0900

    Create 11279.py

---
+++ BOJ/단계별로풀어보기/28.우선순위 큐/11279.py
@@ -0,0 +1,18 @@
+#최대 힙
+
+import heapq as hq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+a = []
+for _ in range(n):
+  x = int(input())
+  if x == 0:
+    if len(a) == 0:
+      print(0)
+    else:
+      print(-hq.heappop(a))
+  else:
+    hq.heappush(a, -x)
